bf26bc49df625417cacff1e93e989da00995a44e
KaterinaD163 <katja_163@mail.ru>
2022-08-18 01:45:42 +0400

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
-}
+// Задание 1
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+            sum += arr[i];
+        }
+            System.out.println("Сумма трат за месяц составила " + sum + "рублей");
+            return arr;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
