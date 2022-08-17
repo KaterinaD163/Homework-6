d67f01d164bf99f0597417e8d7944aa4c5a1aa7f
KaterinaD163 <katja_163@mail.ru>
2022-08-18 02:57:42 +0400

    Первое задание переделанное

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
-        // Первое задание (проба пера)
-        int[] arr = new int[10];
-        int sum = 0;
-        for (int index = 0; index < arr.length; index++) {
-            arr[index] = index + 1;
-            sum += arr[index];
-        }
-        System.out.println(sum);
-    }
 
-
-// Задание 1
-    int[] arr = generateRandomArray();
-
+    // Задание 1
+    // int[] arr = generateRandomArray();
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
-        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-            System.out.println("Сумма трат за месяц составила " + sum + "рублей");
-            return arr;
-        }
-
+        System.out.println("Сумма трат за месяц составила " + sum + "рублей");
     }
+}
+
+
+
+
 
 
 
