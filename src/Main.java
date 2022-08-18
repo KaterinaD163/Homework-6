b123cce2817bf62d7defa13aef5a250324c2ce0e
KaterinaD163 <katja_163@mail.ru>
2022-08-18 15:40:52 +0400

    Два задания выполнено

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         }
         return arr;
     }
+
     public static void main(String[] args) {
         int[] arr = generateRandomArray();
         int sum = 0;
@@ -17,8 +18,25 @@ public class Main {
             sum += arr[i];
         }
         System.out.println("Сумма трат за месяц составила " + sum + "рублей");
+
+        int minCosts = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < minCosts) ;
+            minCosts = arr[i];
+            break;
+        }
+        int maxCosts = -1;
+        for (int a = 0; a < arr.length; a++) {
+            if (arr[a] > maxCosts) ;
+            maxCosts = arr[a];
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minCosts + "рублей." +
+                "Максимальная сумма трат за день составила " + maxCosts + "рублей.");
     }
 }
+
+
+
 
 
 
